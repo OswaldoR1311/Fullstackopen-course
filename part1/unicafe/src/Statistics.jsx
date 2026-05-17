45438bc5b142238bc5503a2730de5b14baaece23
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-05-17 14:05:56 -0400

    Part 1: Exercises 1.7-1.8

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -5,22 +5,52 @@ const Statistics = ({ good, neutral, bad }) => {
     {
       name: 'good',
       value: good,
+      num: 1,
     },
     {
       name: 'neutral',
       value: neutral,
+      num: 0,
     },
     {
       name: 'bad',
       value: bad,
+      num: -1,
     },
   ]
+
+  const totalFeedback = () => {
+    return options.reduce((acum, currentValue) => {
+      return acum + currentValue.value
+    }, 0)
+  }
+
+  const calculateAverage = () => {
+    const total = totalFeedback()
+
+    if (total === 0) {
+      return 0
+    }
+    const pondValue = options.reduce((acum, { value, num }) => {
+      acum += value * num
+      return acum
+    }, 0)
+    return pondValue / total
+  }
+
+  const calculatePositive = () => {
+    return (options[0].value * 100) / totalFeedback()
+  }
+
   return (
     <div>
       <h2>Statistics</h2>
       {options.map(({ name, value }) => (
         <Topic label={name} value={value} key={name} />
       ))}
+      <p>all {totalFeedback()}</p>
+      <p>average {calculateAverage()}</p>
+      <p>positive {calculatePositive()}%</p>
     </div>
   )
 }
