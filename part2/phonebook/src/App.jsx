ee781710c42ffbd35a5c76c6690c1805643d2ddc
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-05-17 23:49:20 -0400

    Part 2: Exercises 2.7-2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,24 +1,53 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [filter, setFilter] = useState('')
 
-  const handleInputChange = (event) => setNewName(event.target.value)
+  const inputRef = useRef(null)
 
   const handleAddPerson = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName }
-    setPersons(persons.concat(newPerson))
-    setNewName('')
+    const newPerson = { name: newName, phone: phone }
+    const findPerson = persons.find((person) => person.name === newPerson.name)
+
+    if (!findPerson) {
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+      setPhone('')
+      inputRef.current.focus()
+    } else {
+      alert(`${newPerson.name} is already added to the phonebook`)
+      setNewName('')
+      setPhone('')
+    }
   }
+
+  const handleInputChange = (event) => setNewName(event.target.value)
+  const handlePhoneChange = (event) => setPhone(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const filterPersonsByName = persons.filter((person) =>
+    person.name.includes(filter),
+  )
+  const filteredList = filter ? filterPersonsByName : persons
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange} />
+      </div>
       <form onSubmit={handleAddPerson}>
+        <h2>add a new</h2>
         <div>
-          name: <input value={newName} onChange={handleInputChange} />
+          name:{' '}
+          <input ref={inputRef} value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          number: <input value={phone} onChange={handlePhoneChange} />
         </div>
         <div>
           <button type='submit'>add</button>
@@ -26,8 +55,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+        {filteredList.map((person, index) => (
+          <li key={index}>
+            {person.name} {person.phone}
+          </li>
         ))}
       </ul>
     </div>
