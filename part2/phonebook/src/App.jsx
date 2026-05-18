b9b6b6ad9e331b3a2e0c078250914f3bfd352991
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-05-18 10:44:49 -0400

    Part 2: Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Heading from './Heading'
 import Filter from './Filter'
 import Form from './Form'
 import PersonList from './PersonList'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [phone, setPhone] = useState('')
   const [filter, setFilter] = useState('')
+
+  const baseUrl = 'http://localhost:3001/persons'
+
+  useEffect(() => {
+    const promise = axios.get(baseUrl)
+    const eventHandler = (response) => {
+      const dataFromServer = response.data
+      setPersons(dataFromServer)
+    }
+    promise.then(eventHandler)
+  }, [])
 
   const filterPersonsByName = persons.filter((person) =>
     person.name.toLowerCase().includes(filter),
