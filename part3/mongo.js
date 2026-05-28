170284407cefa3b183f850a8e999be05d096b2d7
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-05-28 19:16:58 -0400

    .

---
+++ part3/mongo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack-oswaldo:oswaldoemilio@cluster0.wo2v9ry.mongodb.net/PhonebookApp?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+})
