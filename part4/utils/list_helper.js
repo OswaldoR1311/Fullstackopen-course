fbbd316486a212beecd63224f34ee913ace7930f
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-06-03 19:38:02 -0400

    Part 4: Exercises 4.5-4-7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,15 +1,34 @@
-const dummy = blogs => 1
+const lodash = require('lodash')
 
-const totalLikes = blogs => {
-  if (blogs.length === 0) {
-    return 0
-  } else if (blogs.length === 1) {
-    return blogs[0].likes
-  } else {
-    return blogs.reduce((acc, curr) => {
-      return curr.likes + acc
-    }, 0)
-  }
+const dummy = (blogs) => 1
+
+const totalLikes = (blogs) => blogs.reduce((acc, curr) => acc + curr.likes, 0)
+
+const favoriteBlog = (blogs) => lodash.maxBy(blogs, 'likes')
+
+const mostBlogs = (blogs) => {
+	if (blogs.length === 0) return null
+
+	const blogsByAuthor = lodash.groupBy(blogs, 'author')
+	const authorCounts = lodash.map(blogsByAuthor, (authorBlogs, author) => ({
+		author: author,
+		blogs: authorBlogs.length,
+	}))
+
+	return lodash.maxBy(authorCounts, 'blogs')
 }
 
-module.exports = { dummy, totalLikes }
+const mostLikes = (blogs) => {
+	if (blogs.length === 0) return null
+
+	const blogsByAuthor = lodash.groupBy(blogs, 'author')
+
+	const authorLikes = lodash.map(blogsByAuthor, (authorBlogs, author) => ({
+		author: author,
+		likes: lodash.sumBy(authorBlogs, 'likes'),
+	}))
+
+	return lodash.maxBy(authorLikes, 'likes')
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
