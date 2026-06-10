88d08e52166beaa02716f6ad04ee1156122f44b1
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-06-09 22:45:18 -0400

    beggining exercise 4.17

--- part4/models/user.js
+++ part4/models/user.js
@@ -9,6 +9,12 @@ const userSchema = new mongoose.Schema({
 		minLength: 3,
 	},
 	passwordHash: String,
+	blogs: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Blog',
+		},
+	],
 })
 
 userSchema.set('toJSON', {
