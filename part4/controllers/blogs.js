6cc66de44b6c3944dcc97bb3a06af5e198ac9b57
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-06-10 14:10:02 -0400

    Part 4: Exercise 4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,13 +1,20 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 	response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
-	const { title, author, url, likes } = request.body
+	const { title, author, url, likes, userId } = request.body
+
+	const user = await User.findById(userId)
+
+	if (!user) {
+		return response.status(400).json({ error: 'userId missing or not valid' })
+	}
 
 	if (!title || !url) {
 		return response.status(400).json({ error: 'Missing title or URL' })
@@ -18,9 +25,13 @@ blogRouter.post('/', async (request, response) => {
 		author,
 		url,
 		likes,
+		user: user._id,
 	})
 
 	const savedBlog = await newBlog.save()
+	user.blogs = user.blogs.concat(savedBlog._id)
+	await user.save()
+
 	response.status(201).json(savedBlog)
 })
 
