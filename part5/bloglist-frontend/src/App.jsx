f5cafec031c6fe725294de9a36b34cfbf730e90c
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-06-19 21:44:57 -0400

    Begin exercise 5.5

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
 import Footer from './components/Footer'
+import LoginForm from './components/LoginForm'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 import { notificationStatusOptions } from './constants'
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -12,79 +15,24 @@ const App = () => {
 	const [password, setPassword] = useState('')
 	const [notificationMessage, setNotificationMessage] = useState(null)
 	const [notificationStatus, setNotificationStatus] = useState(null)
-	const [title, setTitle] = useState('')
-	const [author, setAuthor] = useState('')
-	const [url, setUrl] = useState('')
-	const [likes, setLikes] = useState(0)
 	const [blogs, setBlogs] = useState([])
 
 	const formLogin = () => (
-		<div>
-			<h2>log in to application</h2>
-			<form onSubmit={handleLogin}>
-				<div>
-					<label>
-						username
-						<input
-							type="text"
-							value={username}
-							onChange={({ target }) => setUsername(target.value)}
-						/>
-					</label>
-				</div>
-				<div>
-					<label>
-						password
-						<input
-							type="password"
-							value={password}
-							onChange={({ target }) => setPassword(target.value)}
-						/>
-					</label>
-				</div>
-				<button type="submit">login</button>
-			</form>
-		</div>
+		<Togglable buttonLabel={'login'}>
+			<LoginForm
+				handleLogin={handleLogin}
+				setUsername={setUsername}
+				setPassword={setPassword}
+				username={username}
+				password={password}
+			/>
+		</Togglable>
 	)
 
 	const blogForm = () => (
-		<div>
-			<h3>Create new blog</h3>
-			<form onSubmit={handleAddBlog}>
-				<div>
-					<label>
-						title
-						<input
-							value={title}
-							type="text"
-							onChange={({ target }) => setTitle(target.value)}
-						/>
-					</label>
-				</div>
-				<div>
-					<label>
-						author
-						<input
-							value={author}
-							type="text"
-							onChange={({ target }) => setAuthor(target.value)}
-						/>
-					</label>
-				</div>
-				<div>
-					<label>
-						url
-						<input
-							value={url}
-							type="url"
-							onChange={({ target }) => setUrl(target.value)}
-						/>
-					</label>
-				</div>
-				<br />
-				<button type="submit">create</button>
-			</form>
-		</div>
+		<Togglable buttonLabel={'cancel'}>
+			<BlogForm />
+		</Togglable>
 	)
 
 	const setNotification = (message, status) => {
@@ -95,14 +43,6 @@ const App = () => {
 			setNotificationMessage(null)
 			setNotificationStatus(null)
 		}, 5000)
-	}
-
-	const cleanInputs = () => {
-		setUsername('')
-		setPassword('')
-		setTitle('')
-		setAuthor('')
-		setUrl('')
 	}
 
 	const handleLogin = async (event) => {
