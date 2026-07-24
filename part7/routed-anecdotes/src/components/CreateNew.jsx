b1eef3d84719336c2651bb38574937b143e5a714
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-07-24 10:30:10 -0400

    Part 7: Exercise 7.1

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,46 +1,63 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useField } from '../hooks'
 
 const CreateNew = ({ addAnecdote }) => {
-  // const [content, setContent] = useState('')
-  // const [author, setAuthor] = useState('')
-  // const [info, setInfo] = useState('')
-  const navigate = useNavigate()
+	// const [content, setContent] = useState('')
+	// const [author, setAuthor] = useState('')
+	// const [info, setInfo] = useState('')
+	const navigate = useNavigate()
 
+	const content = useField('text')
+	const author = useField('text')
+	const info = useField('text')
 
-  const { value: contentValue, onChange: contentChange} = useField('text')
-  const { value: authorValue, onChange: authorChange} = useField('text')
-  const { value: infoValue, onChange: infoChange} = useField('text')
+	function handleSubmit(event) {
+		event.preventDefault()
+		addAnecdote({
+			content: content.value,
+			author: author.value,
+			info: info.value,
+			votes: 0,
+		})
 
-  function handleSubmit(event) {
-    event.preventDefault()
-    addAnecdote({contentValue, authorValue, infoValue, votes: 0})
-    console.log({ contentValue, authorValue, infoValue})
-    navigate('/')
-  }
+		navigate('/')
+	}
 
-
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input name='content' value={contentValue} onChange={contentChange} />
-        </div>
-        <div>
-          author
-          <input name='author' value={authorValue} onChange={authorChange} />
-        </div>
-        <div>
-          url for more info
-          <input name='info' value={infoValue} onChange={infoChange} />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  )
+	return (
+		<div>
+			<h2>create a new anecdote</h2>
+			<form onSubmit={handleSubmit}>
+				<div>
+					content
+					<input
+						type={content.type}
+						name="content"
+						value={content.value}
+						onChange={content.onChange}
+					/>
+				</div>
+				<div>
+					author
+					<input
+						type={author.type}
+						name="author"
+						value={author.value}
+						onChange={author.onChange}
+					/>
+				</div>
+				<div>
+					url for more info
+					<input
+						type={info.type}
+						name="info"
+						value={info.value}
+						onChange={info.onChange}
+					/>
+				</div>
+				<button type="submit">create</button>
+			</form>
+		</div>
+	)
 }
 
 export default CreateNew
