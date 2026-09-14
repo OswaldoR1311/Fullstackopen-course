e9a25b99bd3f589254d9a7ec5227c63bcce37a9f
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-09-14 12:17:26 -0400

    Finishing exercise 3 of chapter 3 of typescript

--- part9/healthapp/bmiCalculator.ts
+++ part9/healthapp/bmiCalculator.ts
@@ -1,41 +1,61 @@
 interface DataBody {
-    weight: number
-    height: number
+	weight: number;
+	height: number;
 }
 
-function calculateBMI(data: DataBody) {
-    const {weight, height} = data
+function parseArguments(args: Array<string>): DataBody {
+	if (args.length < 4) throw new Error("Not enough arguments");
+	if (args.length > 4) throw new Error("Too many arguments");
 
-    if (height <= 0 || weight <= 0) {
-        throw new Error('Weight and height can´t be 0')
-    }
+	if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+		return {
+			weight: Number(args[3]),
+			height: Number(args[2]),
+		};
+	} else {
+		throw new Error("Provided values are not numbers");
+	}
+}
 
-    const heightInCms = height / 100
+function calculateBMI(height: number, weight: number) {
+	if (height <= 0 || weight <= 0) {
+		throw new Error("Weight and height can´t be 0");
+	}
 
-    const bmiOperation = weight / (heightInCms * heightInCms)
+	const heightInCms = height / 100;
 
-    if (bmiOperation < 16) {
-        console.log('Your BMI index is: ' + bmiOperation.toFixed(2) + ' Underweight (Severe thinness)')
-    } else if (bmiOperation >= 16 && bmiOperation <= 17) {
-        console.log('Your BMI index is: ' + bmiOperation.toFixed(2) + ' Underweight (Moderate thinness)')
-    } else if (bmiOperation >= 17 && bmiOperation <= 18.5) {
-        console.log('Your BMI index is: ' + bmiOperation.toFixed(2) + ' Underweight (Mild thinness)')
-    } else if (bmiOperation >= 18.5 && bmiOperation <= 25) {
-         console.log('Your BMI index is: ' + bmiOperation.toFixed(2) + ' Normal range')
-    } else if (bmiOperation >= 25 && bmiOperation <= 30) {
-        console.log('Your BMI index is: ' + bmiOperation.toFixed(2) + ' Overweight (Pre-obese)')
-    } else {
-        console.log('Obese')
-    }
+	const bmiOperation = weight / (heightInCms * heightInCms);
+
+	if (bmiOperation < 16) {
+		console.log(
+			`Your BMI index is: ${bmiOperation.toFixed(2)} Underweight (Severe thinness)`,
+		);
+	} else if (bmiOperation >= 16 && bmiOperation <= 17) {
+		console.log(
+			`Your BMI index is: ${bmiOperation.toFixed(2)}  Underweight (Moderate thinness)`,
+		);
+	} else if (bmiOperation >= 17 && bmiOperation <= 18.5) {
+		console.log(
+			`Your BMI index is: ${bmiOperation.toFixed(2)}  Underweight (Mild thinness)`,
+		);
+	} else if (bmiOperation >= 18.5 && bmiOperation <= 25) {
+		console.log(`Your BMI index is: ${bmiOperation.toFixed(2)}  Normal range `);
+	} else if (bmiOperation >= 25 && bmiOperation <= 30) {
+		console.log(
+			`Your BMI index is: ${bmiOperation.toFixed(2)}  Overweight (Pre-obese) `,
+		);
+	} else {
+		console.log("Obese");
+	}
 }
 
 try {
-    const dataBody: DataBody = { weight: 73, height: 171}
-    calculateBMI(dataBody)
+	const { height, weight } = parseArguments(process.argv);
+	calculateBMI(height, weight);
 } catch (error: unknown) {
-    let errorMsg = 'An error ocurr'
-    if (error instanceof Error) {
-        errorMsg += 'Error: ' + error.message
-    }
-    console.log(errorMsg)
+	let errorMsg = "An error ocurr";
+	if (error instanceof Error) {
+		errorMsg += `Error: ${error.message}`;
+	}
+	console.log(errorMsg);
 }
--- part9/healthapp/exerciseCalculator.ts
+++ part9/healthapp/exerciseCalculator.ts
@@ -1,1 +1,81 @@
 //Code for this exercise
+interface Result {
+	periodLength: number;
+	trainingDays: number;
+	success: boolean;
+	rating: number;
+	ratingDescription: string;
+	target: number;
+	average: number;
+}
+
+interface Msg {
+	rate: number;
+	description: string;
+}
+
+function parseArguments2(args: Array<string>) {
+	if (args.length < 4) throw new Error("Missing arguments");
+
+	args = process.argv.slice(2);
+	const target = Number(args[0]);
+	const dailyHours = args.slice(1).map(Number);
+
+	const isValidHours = dailyHours.every((h) => !Number.isNaN(h));
+
+	if (!Number.isNaN(target) && Array.isArray(dailyHours) && isValidHours) {
+		return {
+			target,
+			dailyHours,
+		};
+	} else {
+		throw new Error("Some of the information is not in a valid format");
+	}
+}
+
+function calculateExercises(dailyHours: Array<number>, target: number): Result {
+	const periodLength = dailyHours.length;
+	const trainingDays = [...dailyHours.filter((day) => day !== 0)].length;
+	const average =
+		dailyHours.reduce((acum: number, currEl: number) => acum + currEl, 0) /
+		periodLength;
+	const success = average > target;
+	const percentage = (average / target) * 100;
+	function getRating(percentage: number): Msg {
+		switch (true) {
+			case percentage >= 90:
+				return {
+					rate: 3,
+					description: "Excelent, you have an outstanding rating",
+				};
+			case percentage >= 60:
+				return { rate: 2, description: "Not too bad but could be better" };
+			case percentage >= 30:
+				return { rate: 1.5, description: "Bad, many things to improve" };
+			default:
+				return { rate: 1, description: "Very bad" };
+		}
+	}
+
+	const { rate, description } = getRating(percentage);
+
+	return {
+		periodLength,
+		trainingDays,
+		average,
+		success,
+		rating: rate,
+		ratingDescription: description,
+		target,
+	};
+}
+
+try {
+	const { dailyHours, target } = parseArguments2(process.argv);
+	console.log(calculateExercises(dailyHours, target));
+} catch (error: unknown) {
+	let errorMsg = "An error ocurr ";
+	if (error instanceof Error) {
+		errorMsg += `Error: ${error.message}`;
+	}
+}
